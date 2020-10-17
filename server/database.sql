c92a82c7fd80d774bff75e24f55cb93dcd198fd0
sung8 <heechuw@gmail.com>
2020-10-16 23:25:40 -0400

    create postgres database, connect server to database

---
+++ server/database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE pernjwt;
+
+--set-extension
+CREATE TABLE users(
+    user_id uuid PRIMARY KEY DEFAULT 
+    uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
+
+--insert fake users
+INSERT INTO users (user_name, user_email, user_password) VALUES ('joe', 'joe123@gmail.com', 'qwerty123');
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+/* sets it up so that anytime I want to access data from client side
+I'm going to have to access acress that body and this allows us to do that access
+
+allows us to access req.body
+*/
+app.use(express.json()); //req.body
+
+// cors allows backend to interact with frontend
+app.use(cors());
+
+// ROUTES //
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});

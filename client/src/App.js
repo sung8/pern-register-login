7eeec68903eb71c19022632abcf35d7932b6c103
sung8 <heechuw@gmail.com>
2020-11-04 23:08:47 -0500

    client side basic components, react router dom, states

---
+++ client/src/App.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useState } from "react";
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+// components
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+/*
+component props and render props
+  we use the render prop because we don't want to remount each time we send prop to a component
+*/
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/register"></Redirect>
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login"></Redirect>
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
